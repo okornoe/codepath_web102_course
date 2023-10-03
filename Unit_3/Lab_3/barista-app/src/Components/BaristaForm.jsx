eb146baa7c3b284a7db5c2a062ad29ca762688f4
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2023-10-03 19:16:39 +0000

    add useState variables

--- Unit_3/Lab_3/barista-app/src/Components/BaristaForm.jsx
+++ Unit_3/Lab_3/barista-app/src/Components/BaristaForm.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const onCheckAnswer = () =>  {console.log("on check answer")};
 const OnNewDrink = () =>  {console.log("on New Drink")};
 
 const BaristaForm = () => {
+    const [inputs, setInputs] = useState({
+        'temperature': '',
+        'milk': '',
+        'syrup': '',
+        'blended': ''
+    });
 
+
+    const ingredients = {
+        'temperature': ['hot', 'lukewarm','cold'],
+        'syrup': ['mocha', 'vanilla', 'toffee', 'maple', 'caramel', 'other', 'none'],
+        'milk': ['cow', 'oat', 'goat', 'almond', 'none'],
+        'blended':['yes', 'turbo', 'no']
+    }
+    
     return (
         <div>
             <h2>Hi, I'd like to order a: </h2>
