58138a1c6751dc81e2c5924cc228cb52d09f2294
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2023-10-11 22:51:42 +0000

    add APIForm

---
+++ Unit_4/Lab_4/Cap/src/components/APIForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const APIForm =({inputs, handleChange, onSubmit}) => {
+  return (
+    <div>
+        <h2>Select Your imahe Attributes:</h2>
+        <form className='form-container'></form>
+    </div>
+  )
+}
+
+export default APIForm;
