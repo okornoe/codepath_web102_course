59e12151c239896b9920f280b3f234baf1f26e8d
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2023-10-12 00:21:43 +0000

    define submitform function call APIForm components

--- Unit_4/Lab_4/Cap/src/App.jsx
+++ Unit_4/Lab_4/Cap/src/App.jsx
@@ -15,11 +15,43 @@ function App() {
     height: "",
   });
 
+  const submitForm = () => {
+    let defaultValues = {
+      format: "jpeg",
+      no_ads: "true",
+      no_cookie_banners: "true",
+      width: "1920",
+      height: "1080",
+    };
+    if (inputs.url == '' || inputs.url == ' ') {
+      alert("You forgot to submit an url!");
+    } else {
+      for (const [key, value] of Object.entries(inputs)) {
+        if (value == '') {
+          inputs[key]  = defaultValues[key]
+        }
+      }
+    }
+  }
+
   return (
     <>
-    <APIForm/>
+    <div className=''>
+      <h1>Build Your Own Screenshot!</h1>
+      <APIForm
+        inputs={inputs}
+        handleChange={(e) =>
+          setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value.trim(),
+          }))
+        }
+        onSubmit={submitForm} 
+      />
+      <br></br>
+    </div>
     </>
-  )
+  );
 }
 
 export default App
